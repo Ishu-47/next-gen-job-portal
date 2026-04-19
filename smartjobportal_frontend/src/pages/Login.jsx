0bd46791b5c1b16ef02e27c4d5bd6936d2c33d42
Ishu-47 <kumarrajoraishu@gmail.com>
2026-04-19 18:36:14 +0530

    feat: enhance login page with improved styling and user guidance

--- smartjobportal_frontend/src/pages/Login.jsx
+++ smartjobportal_frontend/src/pages/Login.jsx
@@ -38,29 +38,47 @@ function Login() {
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-linear-to-br from-blue-50 to-gray-100 px-4">
+            <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-8">
+                <h2 className="text-2xl font-bold text-center mb-2">
+                    Welcome back 👋
+                </h2>
+            
+            <p className="text-sm text-gray-500 text-center mb-6">
+                Login to continue to SmartJobs
+            </p>
             <form
                 onSubmit={handleSubmit}
-                className="bg-white p-6 shadow-md rounded w-96">
-                <h2 className="text-xl font-bold mb-4">Login</h2>
-
+                className="space-y-4">
 
                 <input
                     type="email"
-                    placeholder="Email"
-                    className="border p-2 w-full mb-3"
+                    placeholder="Email Address"
+                    className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-blue-500"
+                    value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    required
                 />
                 <input
                     type="password"
                     placeholder="Password"
-                    className="border p-2 w-full mb-3"
+                    className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-blue-500"
+                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    required
                 />
 
-                <button className="bg-blue-500 text-white p-2 w-full">
-                    Login
+                <button type="submit" disabled={loading}
+                 className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
+                    {loading ? "Logging in.." : "Login"}
                 </button>
             </form>
+            <p className="text-sm text-center mt-4 text-gray-500">
+                Don't have an account?{" "}
+                <span className="text-blue-600 cursor-pointer hover:underline" onClick={()=> navigate("/register")}>
+                    Register
+                </span>
+            </p>
+            </div>
         </div>
     );
 };
